9646375c03d9932f10449a50e49ff960819539b5
Ogieriakhi17 <osazeogieriakhi@gmail.com>
2025-05-15 22:00:31 -0700

    Updated the styles in the tailwind.config

---
+++ app/(roots)/_layout.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <Text>Home inside in the roots</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Home
